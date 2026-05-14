a4807325ae72bc2b31533dc86294d3a506056c42
safeontech <financeiro@4safetecnologia.com>
2026-05-14 17:17:56 +0800

    feat: email+password login, hide token/URL from clients

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,33 +2,26 @@ import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 const _kToken = 'nf_token';
-const _kUrl = 'nf_base_url';
-const _kDefaultUrl = 'http://38.247.130.26';
 
 class AuthProvider extends ChangeNotifier {
   String? _token;
-  String _baseUrl = _kDefaultUrl;
   bool _initialized = false;
 
   String? get token => _token;
-  String get baseUrl => _baseUrl;
   bool get isAuthenticated => _token != null && _token!.isNotEmpty;
   bool get initialized => _initialized;
 
   Future<void> init() async {
     final p = await SharedPreferences.getInstance();
     _token = p.getString(_kToken);
-    _baseUrl = p.getString(_kUrl) ?? _kDefaultUrl;
     _initialized = true;
     notifyListeners();
   }
 
-  Future<void> signIn(String token, {String? baseUrl}) async {
+  Future<void> signIn(String token) async {
     final p = await SharedPreferences.getInstance();
     _token = token;
-    if (baseUrl != null && baseUrl.isNotEmpty) _baseUrl = baseUrl;
     await p.setString(_kToken, token);
-    await p.setString(_kUrl, _baseUrl);
     notifyListeners();
   }
 
@@ -36,13 +29,6 @@ class AuthProvider extends ChangeNotifier {
     final p = await SharedPreferences.getInstance();
     _token = null;
     await p.remove(_kToken);
-    notifyListeners();
-  }
-
-  Future<void> setBaseUrl(String url) async {
-    final p = await SharedPreferences.getInstance();
-    _baseUrl = url;
-    await p.setString(_kUrl, url);
     notifyListeners();
   }
 }
